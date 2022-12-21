5710aba78cbca70a76b460cfb194b5c3ce30b822
neurolinker <andha7x@gmail.com>
2022-12-21 16:02:44 +0700

    - Adding top navigation bar - Adding sidebar navigation - Adding dark and light module - Configure package json script

---
+++ public/app.js
@@ -0,0 +1,53 @@
+const sidebar = document.querySelector("aside");
+const maxSidebar = document.querySelector(".max")
+const miniSidebar = document.querySelector(".mini")
+const roundout = document.querySelector(".roundout")
+const maxToolbar = document.querySelector(".max-toolbar")
+const logo = document.querySelector('.logo')
+const content = document.querySelector('.content')
+const moon = document.querySelector(".moon")
+const sun = document.querySelector(".sun")
+
+function setDark(val){
+    if(val === "dark"){
+        document.documentElement.classList.add('dark')
+        moon.classList.add("hidden")
+        sun.classList.remove("hidden")
+    }else{
+        document.documentElement.classList.remove('dark')
+        sun.classList.add("hidden")
+        moon.classList.remove("hidden")
+    }
+}
+
+function openNav() {
+    if(sidebar.classList.contains('-translate-x-48')){
+        // max sidebar 
+        sidebar.classList.remove("-translate-x-48")
+        sidebar.classList.add("translate-x-none")
+        maxSidebar.classList.remove("hidden")
+        maxSidebar.classList.add("flex")
+        miniSidebar.classList.remove("flex")
+        miniSidebar.classList.add("hidden")
+        maxToolbar.classList.add("translate-x-0")
+        maxToolbar.classList.remove("translate-x-24","scale-x-0")
+        logo.classList.remove("ml-12")
+        content.classList.remove("ml-12")
+        content.classList.add("ml-12","md:ml-60")
+    }else{
+        // mini sidebar
+        sidebar.classList.add("-translate-x-48")
+        sidebar.classList.remove("translate-x-none")
+        maxSidebar.classList.add("hidden")
+        maxSidebar.classList.remove("flex")
+        miniSidebar.classList.add("flex")
+        miniSidebar.classList.remove("hidden")
+        maxToolbar.classList.add("translate-x-24","scale-x-0")
+        maxToolbar.classList.remove("translate-x-0")
+        logo.classList.add('ml-12')
+        content.classList.remove("ml-12","md:ml-60")
+        content.classList.add("ml-12")
+
+    }
+
+}
